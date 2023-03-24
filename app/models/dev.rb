ff35c2f28c35435140c7522e216893ac9dca9fb8
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-24 12:19:20 -0400

    commit tree 595153b02186b1372e24a1835a7b33953ab3e3b6

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies 
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    freebies.any? do |freebie|
+      freebie.item_name == item_name
+    end
+  end
+
+  def give_away(dev, freebie)
+    freebie.update(dev: dev) unless freebie.dev != self  
+  end
 
 end

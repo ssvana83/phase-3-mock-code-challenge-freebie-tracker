3267e4157275019f10872a04043b5b601a1fda36
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-24 12:19:29 -0400

    DONE

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ActiveRecord::Base
+  has_many :freebies 
+  has_many :devs, through: :freebies
 
+  def give_freebie (dev_instance, new_item_name, value_given)
+    Freebie.create(
+      item_name: new_item_name,
+      value: value_given,
+      dev_id: dev_instance.id,
+      company_id: self.id
+    )
+  end
+
+  def self_oldest_company
+    self.order(:founding_year).first
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
+Freebie.destroy_all
+Company.destroy_all
+Dev.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(value: 3, item_name: "key chain", dev_id: d1.id, company_id: c3.id)
+Freebie.create(value: 2, item_name: "frisbee", dev_id: d2.id, company_id: c1.id)
+Freebie.create(value: 12, item_name: "pen", dev_id: d3.id, company_id: c2.id)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
